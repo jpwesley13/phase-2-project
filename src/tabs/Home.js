b9376a2aca6b58dc26547c1d3984b5d4834afe3d
Jared Patterson <jpwesley34@gmail.com>
2024-02-22 06:11:54 -0800

    Creates Monster component.

---
+++ src/tabs/Home.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+import MonsterCard from "../components/MonsterCard";
+import NavBar from "../components/NavBar";
+import MonsterForm from "../components/MonsterForm";
+
+function Home() {
+    return (
+        <>
+          <header>
+            <NavBar />
+          </header>
+        </>
+    )
+};
+
+export default Home;
---
+++ src/tabs/Monster.js
@@ -0,0 +1,16 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "../components/NavBar";
+
+function Monster() {
+
+    return (
+    <>
+        <header>
+            <NavBar />
+        </header>
+    </>
+    );
+};
+
+export default Monster;
