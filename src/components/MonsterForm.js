47d8f5412c5e7dc6860d0e85cb5756507ad8f834
Jared Patterson <jpwesley34@gmail.com>
2024-02-22 10:26:37 -0800

    Builda out monster form component.

--- src/components/MonsterForm.js
+++ src/components/MonsterForm.js
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 
-function MonsterForm() {
+function MonsterForm({onAddMonster}) {
     const [formData, setFormData] = useState({
         name: "",
         series: "",
@@ -9,11 +9,77 @@ function MonsterForm() {
         rarity: "",
         image: "",
     });
+    // const [option, setOption] = useState("The Mind")
 
     function handleChange(e){
+        const {name, value} = e.target
+
         setFormData({
             ...formData,
-            [e.target.name]: e.target.value,
+            [name]: value,
         });
     };
+
+    function handleSubmit(){
+        const monsterData = {
+            name: formData.name,
+            series: formData.series,
+            origin: formData.origin,
+            risk: formData.risk,
+            rarity: formData.rarity,
+            image: formData.image
+        };
+        fetch(`http://127.0.0.1:3000/monster`, {
+            method:"POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(monsterData)
+        })
+        .then(res => res.json())
+        .then(data => onAddMonster(data))
+    };
+
+    return (
+        <div>
+            <h3>Add monster sighting!</h3>
+            <form
+            onSubmit={handleSubmit}>
+                <input label="Name" placeholder="Name" name="name"
+                value={formData.name}
+                onChange={handleChange}
+                />
+                <input label="Series" placeholder="Series" name="series"
+                value={formData.series}
+                onChange={handleChange}
+                />
+                <select 
+                value={formData.origin}
+                onChange={handleChange}
+                name="origin">
+                    <option value="option1">North America</option>
+                    <option value="option2">South America</option>
+                    <option value="option3">Western Eurasia</option>
+                    <option value="option4">East Asia</option>
+                    <option value="option5">Africa</option>
+                    <option value="option6">Oceania</option>
+                    <option value="option7">Poles</option>
+                    <option value="option8">Cyberspace</option>
+                </select>
+                <input label="Risk Factor" placeholder="Risk Factor" name="risk"
+                value={formData.risk}
+                onChange={handleChange}
+                />
+                <input label="Rarity" placeholder="Rarity" name="rarity"
+                value={formData.rarity}
+                onChange={handleChange}
+                />
+                <input label="Image url" placeholder="Image url for sighting" name="image"
+                value={formData.image}
+                onChange={handleChange}
+                />
+                <button>Submit</button>
+            </form>
+        </div>
+    )
 }
