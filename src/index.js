fd8f1d705cc75e1de2d3e89f96885fdc4bf4898e
Jared Patterson <jpwesley34@gmail.com>
2024-02-22 14:45:45 -0800

    Obsoletes Context component for the time being.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import "./index.css";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes";
+import { MonsterProvider } from "./components/Context";
 
 const router = createBrowserRouter(routes)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<RouterProvider router={router} />);
+root.render(
+<RouterProvider router={router}>
+  <MonsterProvider>
+    {router}
+  </MonsterProvider>
+</RouterProvider>);
+
